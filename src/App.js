851bd393d4697bacee1cc4732a73952f19f7dff0
Alexander Hartson <alexander-hartson@live.com>
2018-08-14 01:00:30 -0500

    adding front end code to connect to Spring JDBC

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import CarList from './components/Carlist';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">CarList</h1>
         </header>
+        <CarList />
       </div>
     );
   }
