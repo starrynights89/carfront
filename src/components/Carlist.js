333f45597714ad45299bbb2531b14b9356a6c183
Alexander Hartson <alexander-hartson@live.com>
2018-08-16 17:51:44 -0500

    finishing delete functions

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -3,6 +3,8 @@ import ReactTable from "react-table";
 import 'react-table/react-table.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 import {SERVER_URL} from '../constants.js';
 
 class Carlist extends Component {
@@ -28,6 +30,18 @@ class Carlist extends Component {
     .catch(err => console.error(err));
   }
 
+  confirmDelete = (link) => {
+    confirmAlert({
+      message: 'Are you sure to delete?',
+      buttons: [{
+        label: 'Yes',
+        onClick: () => this.onDelClick(link)
+      }, {
+        label: 'No',
+      }]
+    })
+  }
+
   // Delete car
   onDelClick = (link) => {
     fetch(link, {method: 'DELETE'})
@@ -46,37 +60,41 @@ class Carlist extends Component {
   }
 
   render() {
-    const columns = [{
-      Header: 'Brand',
-      accessor: 'brand',
-    }, {
-      Header: 'Model',
-      accessor: 'model', 
-    }, {
-      Header: 'Color',
-      accessor: 'color',
-    }, {
-      Header: 'Year',
-      accessor: 'year',
-    }, {
-      Header: 'Price $',
-      accessor: 'price',
-    }, {
-      id: 'delbutton',
-      sortable: false,
-      filterable: false,
-      width: 100,
-      accessor: '_links.self.href',
-      Cell: ({value}) => (<button 
-        onClick={()=>{this.onDelClick(value)}}>Delete</button>)
-    }]
+  const columns = [{
+    Header: 'Brand',
+    accessor: 'brand',
+    Cell: this.renderEditable
+  }, {
+    Header: 'Model',
+    accessor: 'model',
+    Cell: this.renderEditable
+  }, {
+    Header: 'Color',
+    accessor: 'color',
+    Cell: this.renderEditable
+  }, {
+    Header: 'Year',
+    accessor: 'year',
+    Cell: this.renderEditable
+  }, {
+    Header: 'Price €',
+    accessor: 'price',
+    Cell: this.renderEditable
+  }, {
+    id: 'delbutton',
+    sortable: false,
+    filterable: false,
+    width: 100,
+    accessor: '_links.self.href',
+    Cell: ({value}) => (<button onClick={()=>{this.confirmDelete(value)}}>Delete</button>)
+  }]
 
-    return (
-      <div className="App">
-        <ReactTable data={this.state.cars} 
-          columns={columns} filterable={true}/>
-        <ToastContainer autoClose={1500} />
-      </div>
+  return (
+    <div className="App">
+      <ReactTable data={this.state.cars} columns={columns} 
+        filterable={true} pageSize={10}/>
+      <ToastContainer autoClose={1500}/>  
+    </div>
     );
   }
 }
